5f2cb4540a2cfe3d2ca862c964a0e1c36a3a3e5c
victorandresvega <victor.vega6@upr.edu>
2021-02-05 10:31:38 -0400

    newfunction

--- Practica.cpp
+++ Practica.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
+bool isLower(int first, int second){
+    if(first > second){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
 int main(){
     cout<<"edit"<<endl;
     cout<<"new change";
